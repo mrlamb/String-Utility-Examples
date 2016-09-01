4823e89ab3ec3e56e48e823bc4b4ddf9b85b22df
MRLamb <lamb.matthew@gmail.com>
2016-09-01 18:12:04 -0500

    First Commit, two methods and an example unit test

---
+++ MRLamb.StringUtils/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRLamb.StringUtils
+{
+    public static class Utils
+    {
+        public static string TrimInnerWhitespace(this string str)
+        {
+            str = str.Trim();
+            int index;
+            index = str.IndexOf("  ");
+            if (index < 0)
+                return str;
+            else
+                return (str.Substring(0, index + 1) + str.Substring(index + 2)).TrimInnerWhitespace();
+         }
+
+        public static bool IsNullOrWhitespace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+       
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MRLamb.StringUtils;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void StrReturnsTrimmedString()
+        {
+            string test = "On    top of       spaghetti         ";
+            Assert.AreEqual("On top of spaghetti", test.TrimInnerWhitespaceReplace());
+        }
+    }
+}
